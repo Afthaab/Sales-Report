ecebf06552a202b04bb5959a81324cebb4af7b42
afthab <mohammedafthab15@gmail.com>
2025-03-26 23:31:57 +0530

    dev: adding the database connection; importing zerlog and godotenv pacakages

--- cmd/app/sales_report_lumel/main.go
+++ cmd/app/sales_report_lumel/main.go
@@ -1,1 +1,22 @@
-package salesreportlumel
+package main
+
+import (
+	"fmt"
+
+	database "github.com/Afthaab/Sales-Report-Lumel/internal/database/postgres"
+	"github.com/rs/zerolog/log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal().AnErr("error", err).Msg("faile to load the env file")
+	}
+}
+
+func main() {
+	db := database.ConnectToDatabase()
+	fmt.Println(db)
+}
--- internal/database/postgres/connection.go
+++ internal/database/postgres/connection.go
@@ -1,1 +1,25 @@
-package postgres
+package database
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectToDatabase() *gorm.DB {
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		log.Fatal().AnErr("error", err).Msg("failed to connect to the database")
+	} else {
+		log.Info().Msg("successfully connected to the database")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal().AnErr("error", err).Msg("failed to get the database instance")
+	}
+	defer sqlDB.Close()
+	return db
+}
